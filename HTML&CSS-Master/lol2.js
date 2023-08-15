b403dceaeba3429b88276d52eda58bf3edd9c336
Dugo220203tg <141121686+Dugo220203tg@users.noreply.github.com>
2023-08-15 21:13:19 +0700

    Add files via upload

--- HTML&CSS-Master/lol2.js
+++ HTML&CSS-Master/lol2.js
@@ -9,27 +9,27 @@ const array = [
   "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0157/ability_0157_P1.webm",
 ]
 
-const $ = document.querySelector.bind(,ocument);
+const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$(".tab-img");
 const panes = $$(".tab-pane");
-// const videos = $$(".tab-video");
+const videos = $$(".tab-video");
 
 const tabActive = $(".tab-img.active");
 requestIdleCallback(function () {});
 
 tabs.forEach((tab, index) => {
   const pane = panes[index];
-  // const video = videos[index];
+  const video = videos[index];
   tab.onclick = function () {
     $(".tab-img.active").classList.remove("active");
     $(".tab-pane.active").classList.remove("active");
-    // $(".tab-video.active").classList.remove("active");
+    $(".tab-video.active").classList.remove("active");
 
     this.classList.add("active");
     pane.classList.add("active");
-    // video.classList.add("active");
+    video.classList.add("active");
   };
 });
 
